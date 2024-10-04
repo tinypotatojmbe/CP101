6bdd2f71e25547d96c91bc6582166ee845bb3c65
tinypotatojmbe <jolissae@gmail.com>
2024-10-05 01:23:16 +0700

    Create Elcamel.py

---
+++ Activity 1/Elcamel.py
@@ -0,0 +1,11 @@
+# Find the value of variable a.
+a = 2 + 3 * 4 - 5 / 2
+print(a)
+
+# Find the value of variable b.
+b = 2 * a / 3 ** 2
+print(b)
+
+# Find the value of variable c.
+c = a + b
+print(c)
